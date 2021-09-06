d7415b798a8e0dfff2416813e304bbd40a5bd848
zenius <zenius@outlook.com>
2021-09-06 10:55:16 +0800

    Feat: Check for utility connector

--- checker/connectors.py
+++ checker/connectors.py
@@ -8,25 +8,36 @@ def problem_line(conn: Connector, problem: str) -> dict:
         "problem": problem,
         "target": conn.handle,
         "number": conn.number,
-        "drawing": conn.drawing.number,
+        "drawing": get_dwg_num(conn),
         "location": (round(conn.location.x, 2), round(conn.location.y, 2))
     }
 
 
 def number_matched(conn: Connector) -> bool:
     # return conn.number[:-2] == conn.drawing.number[1:]
-    return int(conn.number[:-2]) == int(conn.drawing.number)
+    return int(conn.number[:-2]) == int(get_dwg_num(conn))
 
 
 def is_excluded(conn: Connector) -> bool:
     # exclude legend drawings
-    return int(conn.drawing.number) < 10
+    return int(get_dwg_num(conn)) < 10
+
+
+def get_dwg_num(conn: Connector) -> str:
+    return conn.drawing.id[-4:]
 
 
 def check(p: PnID):
+    pretty = PrettyPrinter()
+    print("MainConnectors:")
+    pretty.pprint(check_main(p))
+    print("UtilityConnectors:")
+    pretty.pprint(check_utility(p))
+
+
+def check_main(p: PnID) -> list:
     connectors = p.main_connectors
     problems = []
-    pretty = PrettyPrinter()
     for connector in connectors:
         if is_excluded(connector):
             continue
@@ -45,4 +56,29 @@ def check(p: PnID):
         elif connector.is_off_drawing & (not bool(connector.link_drawing)):
             problems.append(problem_line(connector, "Missing P&ID No. in off-drawing conn"))
     print(f"{len(problems)} problems detected:")
-    pretty.pprint(problems)
+    return problems
+
+
+def check_basic(connector: Connector) -> list:
+    problems = []
+    if not connector.number:
+        problems.append(problem_line(connector, "Missing tag number"))
+
+    print(f"{len(problems)} problems detected:")
+    return problems
+
+
+def check_utility(p: PnID) -> list:
+    problems = []
+    for connector in p.utility_connectors:
+        if is_excluded(connector):
+            continue
+        if not connector.number:
+            problems.append(problem_line(connector, "Missing number"))
+
+    return problems
+
+
+if __name__ == "__main__":
+    p = PnID()
+    check(p)
