6c19b2277e5965648dad73dd6b8ce2a813d046fc
zenius <zenius@outlook.com>
2021-10-28 10:19:05 +0800

    feat: switch font style from eng to chs

--- scripts/lang_switch.py
+++ scripts/lang_switch.py
@@ -3,7 +3,33 @@ from pnid import PnID
 
 
 def switch_to_chs(p: PnID):
-    pass
+    """
+    Switch language style from english to chinese.
+    Including text style and some translation
+    :param p:
+    :return:
+    """
+    print("-> CHS")
+    print("Start with connectors")
+    counter = 0
+    for connector in p.main_connectors + p.utility_connectors:
+        # Exclude symbol legend
+        if int(connector.drawing.tag) > 10:
+            set_to_chs(connector.service_attr)
+            if hasattr(connector, "route_attr"):
+                set_to_chs(connector.route_attr)
+                # translation
+                connector.route = connector.route.replace("TO ", "至 ")
+                connector.route = connector.route.replace("FROM ", "自 ")
+            counter += 1
+
+    print(f"{counter} connectors switched.")
+
+
+def set_to_chs(attr_ref):
+    attr_ref.StyleName = "ConnectorText_CHS"
+    attr_ref.Height = 4
+    attr_ref.ScaleFactor = 1
 
 
 if __name__ == "__main__":
