d582bcee167df77f9a2a739ff0f346bf2ee5c728
zenius <zenius@outlook.com>
2022-02-08 17:01:31 +0800

    init script

---
+++ scripts/diagram_maker.py
@@ -0,0 +1,11 @@
+# Generate a diagram showing relationship of drawings and connectors
+from pnid import PnID
+
+
+def main(pid: PnID, outfile):
+    pass
+
+
+if __name__ == '__main__':
+    pid = PnID()
+    main(pid)
